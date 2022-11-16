3c52d77b69614e421520a6ad04bd4d504c7eb864
cpm-128 <101371032+cpm-128@users.noreply.github.com>
2022-11-16 10:25:11 -0500

    notes about what each function should do

--- Unsolved/arithmetic.js
+++ Unsolved/arithmetic.js
@@ -1,9 +1,13 @@
 // Fill in the body of each function with the code required
 
+//TODO: returns the result of num1 plus num2
 var add = function(num1, num2) {};
 
+//TODO: returns the result of num1 minus num2
 var subtract = function(num1, num2) {};
 
+//TODO: returns the result of num1 times num2
 var multiply = function(num1, num2) {};
 
+//TODO: returns the result of num1 divided by num2
 var divide = function(num1, num2) {};
