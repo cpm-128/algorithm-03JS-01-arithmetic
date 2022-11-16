e927f74db3f7e30ead295839c311559352537ef3
cpm-128 <101371032+cpm-128@users.noreply.github.com>
2022-11-16 10:28:30 -0500

    multiply passes

--- Unsolved/arithmetic.js
+++ Unsolved/arithmetic.js
@@ -12,7 +12,9 @@ var subtract = function(num1, num2) {
 };
 
 //TODO: returns the result of num1 times num2
-var multiply = function(num1, num2) {};
+var multiply = function(num1, num2) {
+    return num1 * num2
+};
 
 //TODO: returns the result of num1 divided by num2
 var divide = function(num1, num2) {};
